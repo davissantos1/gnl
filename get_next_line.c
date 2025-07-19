952ce389b586c3ab8fc89f3a5274342d4fe3810a
Davi Simoes Dos Santos <dasimoes@c1r5p4.42sp.org.br>
2025-07-18 23:30:30 -0300

    gnl improved

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 22:04:41 by dasimoes          #+#    #+#             */
+/*   Updated: 2025/07/18 23:30:03 by dasimoes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -6,6 +18,8 @@ char	*get_next_line(int fd)
 	int	bytes_read;
 
 	bytes_read = 1;
-	while (bytes_read)
+	while (!(ft_strchr(buffer, '\n')) && bytes_read)
+	{
+		
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasimoes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 22:04:53 by dasimoes          #+#    #+#             */
+/*   Updated: 2025/07/18 22:09:13 by dasimoes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -6,11 +18,12 @@
 # include <fcntl.h>
 
 // Macro default definition
-#ifndef BUFFER_SIZE
+# ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
-#endif
+
+# endif
 
 // Function prototypes
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
